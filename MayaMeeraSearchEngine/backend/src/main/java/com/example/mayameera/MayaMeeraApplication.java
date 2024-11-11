879174554a756b1b5ac1ead64149cac02ab9c6f9
KashyapSinh-Gohil <gohilkashyapsinh@amityonline.com>
2024-11-11 16:37:30 +0530

    Update MayaMeeraApplication.java

--- MayaMeeraSearchEngine/backend/src/main/java/com/example/mayameera/MayaMeeraApplication.java
+++ MayaMeeraSearchEngine/backend/src/main/java/com/example/mayameera/MayaMeeraApplication.java
@@ -1,5 +1,11 @@
-public class MayaMeeraApplication { 
-"    public static void main(String[] args) {" 
-"        SpringApplication.run(MayaMeeraApplication.class, args);" 
-"    }" 
-} 
+package com.example.mayameera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayaMeeraApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MayaMeeraApplication.class, args);
+    }
+}
